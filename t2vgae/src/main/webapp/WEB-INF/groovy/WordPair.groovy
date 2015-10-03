3a7b456264a69e4e36b500b4e6ae4b14135a9a45
Laurie <lauriehirsch@gmail.com>
2015-10-03 13:18:37 +0100

    new class for wordpairs

---
+++ t2vgae/src/main/webapp/WEB-INF/groovy/WordPair.groovy
@@ -0,0 +1,12 @@
+import groovy.transform.Canonical
+
+@Canonical
+class WordPair {
+    String word0
+    String word1
+    def cooc
+ //   int wordFreq
+    // def sortValue
+    //int word0Freq
+    //int word1Freq
+}
